767f2e5903fab8cd158a899722e2989698161b7a
Ramadhan Febri Utama <rafeta02@student.uns.ac.id>
2025-01-09 11:15:33 +0700

    ganti icon

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -9,7 +9,7 @@
 
     <title>@yield('title', 'Akreditasi UNS | LPPMP UNS')</title>
 
-    {{-- <link rel="shortcut icon" href="{{ asset('jobcy/images/favicon.ico') }}"> --}}
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
